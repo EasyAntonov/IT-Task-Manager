903f8a02b716f76f4c22226c6f1527d85874550b
Antonov Andrew <prestigemilitary@gmail.com>
2025-08-15 17:12:33 +0300

    add projecе models

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,65 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    teams = models.ManyToManyField(Team, related_name='projects')
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name='members')
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("Urgent", "Urgent"),
+        ("High", "High"),
+        ("Medium", "Medium"),
+        ("Low", "Low"),
+    ]
+
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(choices=PRIORITY_CHOICES, max_length=20, default="Medium")
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="tasks",
+    )
+    assignees = models.ManyToManyField(Worker, related_name='assignees')
+    tags = models.ManyToManyField(Tag, related_name='tasks', blank=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, related_name='tasks')
