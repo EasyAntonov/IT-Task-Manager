cc123f8abfccd238060f6dfd622d17338be8f30f
Antonov Andrew <prestigemilitary@gmail.com>
2025-08-21 18:10:51 +0300

    add Task view, index view and percentage calculate function

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,97 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from task_manager.models import Task, Worker, Position, Project
+
+
+@login_required
+def index(request):
+    num_tasks = Task.objects.count()
+    num_workers = Worker.objects.count()
+    num_positions = Position.objects.count()
+    num_projects = Project.objects.count()
+    num_completed_tasks = Task.objects.filter(completed=True).count()
+    num_incomplete_tasks = Task.objects.filter(completed=False).count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_tasks": num_tasks,
+        "num_workers": num_workers,
+        "num_positions": num_positions,
+        "num_projects": num_projects,
+        "num_completed_tasks": num_completed_tasks,
+        "num_incomplete_tasks": num_incomplete_tasks,
+        "num_visits": num_visits,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
+
+
+def calculate_completed_percentage(completed_percentage):
+    total_tasks = Task.objects.count()
+    completed_tasks = Task.objects.filter(completed=True).count()
+
+    if total_tasks > 0:
+        percentage = completed_tasks / total_tasks * 100
+    else:
+        percentage = 0
+
+    return int(percentage)
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    template_name = "task_manager/task_list.html"
+    context_object_name = "task_list"
+    paginate_by = 10
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        completed_percentage = calculate_completed_percentage()
+        context["completed_percentage"] = completed_percentage
+
+        name = self.request.GET.get("name", "")
+        context["search_form"] = TaskSearchForm(initial={"name": name})
+
+        return context
+
+    def get_queryset(self):
+        queryset = Task.objects.all().order_by("-id")
+        form = TaskSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
+
+
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Task
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    template_name = "task_manager/task_confirm_delete.html"
+    success_url = reverse_lazy("task_manager:task-list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        task = self.get_object()
+        context["related_workers"] = task.assignees.all()
+        return context
+
