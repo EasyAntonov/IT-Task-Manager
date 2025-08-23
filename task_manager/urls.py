40c7abe3af1b5e3f7b605d5384d8d19018a9564a
Antonov Andrew <prestigemilitary@gmail.com>
2025-08-23 21:01:15 +0300

    UserTasks path to urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from task_manager.views import index, TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView, \
-    CompletedTaskListView
+    CompletedTaskListView, UserTaskListView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -11,4 +11,5 @@ urlpatterns = [
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("completed-tasks/", CompletedTaskListView.as_view(), name="completed-task-list"),
+    path("tasks/assigned", UserTaskListView.as_view(), name="user-tasks"),
 ]
