0c13631b27580c8d41c136c3023add8e3e034b5b
Antonov Andrew <prestigemilitary@gmail.com>
2025-08-23 20:46:53 +0300

    Add index path and task paths to urls

---
+++ task_manager/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from task_manager.views import index, TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView, \
+    CompletedTaskListView
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("completed-tasks/", CompletedTaskListView.as_view(), name="completed-task-list"),
+]
