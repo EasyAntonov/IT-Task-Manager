6a1e7f40f03bdd200318a1cce051a9ee7078664f
Antonov Andrew <prestigemilitary@gmail.com>
2025-08-23 21:43:10 +0300

    Add TaskType form

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+from task_manager.models import Tag, Task
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(
@@ -51,3 +53,27 @@ class SingUpForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ("username", "password1", "password2")
+
+
+class TaskForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Task
+        fields = [
+            "name",
+            "description",
+            "deadline",
+            "assignees",
+            "priority",
+            "task_type",
+            "tags",
+            "project",
+        ]
