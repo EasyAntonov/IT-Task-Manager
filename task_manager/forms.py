4f4f61bcdb848e7ead577454e5482a7148e2967d
Antonov Andrew <prestigemilitary@gmail.com>
2025-08-23 21:37:25 +0300

    Add SingUp form

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
 
 class LoginForm(forms.Form):
@@ -18,3 +20,34 @@ class LoginForm(forms.Form):
             }
         )
     )
+
+
+class SingUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "form-control",
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control",
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Confirm Password",
+                "class": "form-control",
+            }
+        )
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ("username", "password1", "password2")
