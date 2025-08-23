575335e1ad7e7ee19c9437cffa4959e2dcf025ff
Antonov Andrew <prestigemilitary@gmail.com>
2025-08-23 21:05:02 +0300

    Add Worker paths to urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from task_manager.views import index, TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView, \
-    CompletedTaskListView, UserTaskListView
+    CompletedTaskListView, UserTaskListView, WorkerListView, WorkerDetailView, WorkerCreateView, WorkerUpdateView, \
+    WorkerDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -12,4 +13,9 @@ urlpatterns = [
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("completed-tasks/", CompletedTaskListView.as_view(), name="completed-task-list"),
     path("tasks/assigned", UserTaskListView.as_view(), name="user-tasks"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("workers/create/", WorkerCreateView.as_view(), name="worker-create"),
+    path("workers/update/", WorkerUpdateView.as_view(), name="worker-update"),
+    path("workers/delete/", WorkerDeleteView.as_view(), name="worker-delete"),
 ]
